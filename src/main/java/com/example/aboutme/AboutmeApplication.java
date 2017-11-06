e97c577447df372fe56cf84d2612064283f46527
yemisrach <meserlengereh@gmail.com>
2017-11-06 16:54:39 -0500

    Initial commit

---
+++ src/main/java/com/example/aboutme/AboutmeApplication.java
@@ -0,0 +1,12 @@
+package com.example.aboutme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AboutmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AboutmeApplication.class, args);
+	}
+}
